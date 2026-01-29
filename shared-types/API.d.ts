41fafbee5921919f455fb0a9cfe3dc3a7fb04738
Gustavo Lima da Silva <gustavolimadasilva50@gmail.com>
2026-01-29 15:33:06 -0300

    Configure shared-types as a TypeScript project and reference it from the server for type sharing

---
+++ shared-types/API.d.ts
@@ -0,0 +1,40 @@
+export interface ProfileData {
+    uid: string;
+    level: number;
+}
+interface ApiResponse {
+    name: string;
+    id: string;
+}
+export interface LessonType extends ApiResponse {
+    video_url: string;
+    explanation: string;
+    video_script: string;
+    level: number;
+    flashcard_deck: DeckType;
+}
+export interface DecksType {
+    lesson_decks: DeckType[];
+    personal_decks: DeckType[];
+}
+export interface DeckType extends ApiResponse {
+    deckDescription: string | null;
+    cards: FlashcardType[];
+}
+export type FlashcardType = (FlashcardTypeBase & {
+    ImageUrl: string;
+}) | (FlashcardTypeBase & {
+    cardFront: string;
+});
+export declare const Opts: readonly ["a", "b", "c", "d"];
+interface FlashcardTypeBase {
+    cardType: 'image' | 'written';
+    imageFile?: FileList | {};
+    imageUrl?: string;
+    cardFront?: string;
+    correct_answer?: typeof Opts[number];
+    options: {
+        [prop in typeof Opts[number]]: string;
+    };
+}
+export {};
